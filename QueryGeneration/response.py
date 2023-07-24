10d76d85d7740a5c621fd87da097a700f8d46897
Naman Chhibbar <n.chhibar@gmail.com>
2023-07-24 11:07:57 +0530

    created new function generate_table_desc in generate.py

--- QueryGeneration/response.py
+++ QueryGeneration/response.py
@@ -1,50 +1,32 @@
 import os, openai
 from dotenv import load_dotenv
-from mysql.connector import connect
 from json import loads
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from source import configs
-from source.generate import generate_sql
+from source.generate import generate_sql, generate_table_desc
 
 @csrf_exempt
 def respond(request):
 
     load_dotenv(configs.PATH_TO_ENV)
     body = loads(request.body)
-
     openai.api_key = os.getenv("OPENAI_KEY")
-    db_user = os.getenv("MYSQL_USER")
-    db_key = os.getenv("MYSQL_KEY")
-    db_host = os.getenv("MYSQL_HOST")
-    db = os.getenv("MYSQL_DB")
 
-    user_query = body["user_query"]
-    table_list = body["tables"]
-
-    connection = connect(
-        user=db_user,
-        password=db_key,
-        host=db_host,
-        db=db
+    tables = generate_table_desc(
+        table_list=body["tables"],
+        user=os.getenv("MYSQL_USER"),
+        key=os.getenv("MYSQL_KEY"),
+        host=os.getenv("MYSQL_HOST"),
+        db=os.getenv("MYSQL_DB")
     )
-    cur = connection.cursor()
 
-    tables = []
-    for table in table_list:
-        table_dict = {}
-        table_dict["name"] = table
-        cur.execute(f"DESC {table}")
-        table_dict["desc"] = cur.fetchall()
-        cur.execute(f"SELECT * from {table} LIMIT 3")
-        table_dict["samples"] = cur.fetchall()
-        tables.append(table_dict)
+    message, usage = generate_sql(
+        query=body["user_query"],
+        tables_desc=tables
+    )
 
-    message, usage = generate_sql(user_query, tables)
-
-    cur.close()
-    connection.close()
     return JsonResponse({
         "success": True,
         "message": message,
--- source/generate.py
+++ source/generate.py
@@ -1,9 +1,11 @@
+from mysql.connector import connect
+
 from . import configs
 from .openai_call import chat_completion_call
 
-def generate_sql(query, tables: list[dict]):
+def generate_sql(query, tables_desc: list[dict]):
     prompt = ""
-    for table in tables:
+    for table in tables_desc:
         prompt += f"Table name: {table['name']}\n\nTable schema:\n"
         for attribute in table["desc"]:
             prompt += f"{attribute}\n"
@@ -20,3 +22,26 @@ def generate_sql(query, tables: list[dict]):
         sys_msg=configs.SYSTEM_MESSAGE,
         prev_msgs=configs.PREVIOUS_MESSAGES
     )
+
+def generate_table_desc(table_list, user, key, host, db):
+    connection = connect(
+        user=user,
+        password=key,
+        host=host,
+        db=db
+    )
+    cur = connection.cursor()
+
+    tables_desc = []
+    for table in table_list:
+        table_dict = {}
+        table_dict["name"] = table
+        cur.execute(f"DESC {table}")
+        table_dict["desc"] = cur.fetchall()
+        cur.execute(f"SELECT * from {table} LIMIT 3")
+        table_dict["samples"] = cur.fetchall()
+        tables_desc.append(table_dict)    
+
+    cur.close()
+    connection.close()
+    return tables_desc
